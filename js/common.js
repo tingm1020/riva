499f5c97f44f2673c571ed3f32a5cea2f9732be2
tingm1020 <58199984+tingm1020@users.noreply.github.com>
2020-11-27 19:14:41 +0800

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,6 +1,27 @@
 var w = window.innerWidth;
 
 $(document).ready(function(){
+    var swiper = new Swiper('.swiper-container',{
+        speed:400,
+        parallax: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            prevEl: '.swiper-button-next',
+            nextEl: '.swiper-button-prev',
+        }
+    });
+    $('.header .block .icon').click(function(){ 
+        if($(this).hasClass('on')){
+            $(this).removeClass('on');
+            $('.header .menu').removeClass('on');
+        }else{
+            $(this).addClass('on');
+            $('.header .menu').addClass('on');
+        } 
+    });
     $('.footer .box .title').click(function(){
         if($(this).hasClass('on')){
             $(this).removeClass('on');
@@ -12,11 +33,11 @@ $(document).ready(function(){
     });
     if(w >= 750){
         if($('.footer .box .cont').css('display') == "none"){
-            console.log('test width');
             $('.footer .box .cont').addClass('on');
         }else{
             $('.footer .box .cont').removeClass('on');
         }
     }
 });
+
 
